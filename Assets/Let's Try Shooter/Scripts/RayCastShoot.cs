53fcabe999cf7554d5993cef96f8e1a520044191
Dusk <65917000+duskdev17@users.noreply.github.com>
2021-06-28 00:22:50 +0600

    update

--- Assets/Let's Try Shooter/Scripts/RayCastShoot.cs
+++ Assets/Let's Try Shooter/Scripts/RayCastShoot.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class RayCastShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int gunDamage = 1;
+    public float fireRate = 0.25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.7f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
     void Start()
     {
-        
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKeyDown("Fire1") && Time.time > nextFire) { 
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        }               
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
     }
 }
